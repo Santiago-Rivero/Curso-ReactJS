79abef5a222ada4660b64d429117060513abebda
Santiago-Rivero <santiagonrivero@gmail.com>
2022-08-18 22:22:55 -0300

    conect to firebase/firestore

--- src/components/layout/home/ItemListContainer.jsx
+++ src/components/layout/home/ItemListContainer.jsx
@@ -3,36 +3,38 @@ import { useParams } from 'react-router-dom';
 import LoaderCircular from "../../shared/LoaderCircular";
 import ItemList from "./ItemList/ItemList";
 
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
+
 const ItemListContainer = () =>{
     const [products, setProducts] = useState([]);
     const [loader, setLoader] = useState(true)
     const { idCategory } = useParams();
 
     useEffect(() => {
-        let arrayProducts = [
-            {id: 100, title: "Short de futbol", price: 35, categoria: 'futbol', pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_763145-MLA32645967609_102019-W.jpg", stock:6},
-            {id: 101, title: "Short de rugby", price: 40, categoria: 'rugby', pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_691936-MLA43986811943_112020-V.jpg", stock:4},
-            {id: 102, title: "Calza ciclista", price: 29, categoria: 'ciclismo', pictureUrl: "https://metasports.com.py/media/catalog/product/cache/b2a5ac4ab402bf1fe90b276737a2a112/high_low/apparel/HILOWSC010-1.jpg", stock:5},
-            {id: 104, title: "Short pollera", price: 30, categoria: 'gimnasio', pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_987674-MLA31709255457_082019-O.jpg", stock:3},
-            {id: 105, title: "Remera deportiva", price: 30, categoria: 'gimnasio', pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_821595-MLA44444960415_122020-W.jpg", stock:3},
-            {id: 106, title: "Media Futbol", price: 30, categoria: 'futbol', pictureUrl: "https://cf.shopee.com.ar/file/b8d13ccda2741d35424cda797edf2c5b_tn", stock:3},
-        ]
-
-        const promiseProducts = new Promise((res, rej) =>{
-            setLoader(true)
-            setTimeout(() =>{
-                if(!idCategory){
-                    res (arrayProducts);
-                }else{
-                    res(arrayProducts.filter((product)=> product.categoria === idCategory));
-                }
-                setLoader(false)
-            },1000)
+        console.log('idCategory', idCategory);
+        const db = getFirestore();
+        const refToProducts = collection(db, 'products')
+        
+        getDocs(refToProducts).then(res => {
+            setProducts(res.docs.map( doc => ({id: doc.id, ...doc.data()})))
+        }).finally(() =>{
+            setLoader(false);
         })
 
-        promiseProducts.then( res =>{
-            setProducts(res);
-        });
+        if(idCategory){
+            setLoader(true);
+            const q = query(collection(db, 'products'), where('category', '==', idCategory));
+            getDocs(q).then(res => {
+                if(res.size == 0){
+                    console.log('error');
+                }
+                setProducts(res.docs.map( doc => ({id: doc.id, ...doc.data()})))
+            }).finally(setLoader(false))
+
+        }
+
+        
+
     }, [idCategory]);
 
     return(
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,19 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyCIihhyrN8DejWgnZNgmoU1JvNtzVTP0",
+  authDomain: "ecommerce-curso-coder.firebaseapp.com",
+  projectId: "ecommerce-curso-coder",
+  storageBucket: "ecommerce-curso-coder.appspot.com",
+  messagingSenderId: "216777360401",
+  appId: "1:216777360401:web:0261e0aebab4062836ddb0"
+};
+
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
   {/* <React.StrictMode> */}
